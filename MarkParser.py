4ca8dda6521774716f455c4d1a8d82e3dd53996c
root <Zao0oza>
2022-07-11 14:37:50 +0300

    update

--- MarkParser.py
+++ MarkParser.py
@@ -2,7 +2,6 @@ import argparse
 import xml.etree.cElementTree as ET
 import json
 
-
 parser = argparse.ArgumentParser(description='парсер дампа ebus в json формате, выделяет комманды из мануала, '
                                              'на выходе список исползованных комманд и список коммнад с timestmap')
 parser.add_argument('-dump', type=str, default='ebus.json', help='путь к файлу с дампом')
@@ -20,21 +19,35 @@ with open(args.dump) as f:
 
 def parser(dumps, tree):
     for i in dumps:
+        res=''
+        value=None
         if i['_source']["layers"].get("gvcp", False):
             for j in i['_source']["layers"]["gvcp"].keys():
-                if "Command Header" in j:
+                if "Command Header" or 'Acknowledge Header' in j:
                     if i['_source']["layers"]["gvcp"][j].get("gvcp.bootstrap.custom.register.write", False):
                         res = i['_source']["layers"]["gvcp"][j]["gvcp.bootstrap.custom.register.write"]
-                        res = res[:2] + res[-4::].upper()
-                        text_reg = './/*[{http://www.genicam.org/GenApi/Version_1_0}Address="%s"]' % res
-                        result_reg = tree.find(text_reg)
-                        if result_reg:
-                            text = './/*[{http://www.genicam.org/GenApi/Version_1_0}Integer]/*[@Name="%s"]' % result_reg.get(
-                                "Name")[0:-3]
-                            result = tree.find(text)
-                            if result:
-                                command_dict[result.get("Name")] = {x.tag[43:]: x.text for x in tree.find(text)}
-                                timestamp_dict[i['_source']["layers"]["frame"]['frame.time_relative']] = [result.get("Name"),command_dict[result.get("Name")]['Description']]
+                    elif i['_source']["layers"]["gvcp"][j].get("gvcp.bootstrap.custom.register.read", False):
+                        res = i['_source']["layers"]["gvcp"][j]["gvcp.bootstrap.custom.register.read"]
+                    else:
+                        pass
+                    res = res[:2] + res[-4::].upper()
+                    text_reg = './/*[{http://www.genicam.org/GenApi/Version_1_0}Address="%s"]' % res
+                    result_reg = tree.find(text_reg)
+                    if result_reg:
+                        text = './/*[{http://www.genicam.org/GenApi/Version_1_0}Integer]/*[@Name="%s"]' % result_reg.get(
+                            "Name")[0:-3]
+                        result = tree.find(text)
+                        if result:
+                            if 'WRITEREG' in j.split(':')[1]:
+                                value=i['_source']["layers"]["gvcp"][j].get("gvcp.bootstrap.custom.register.write_tree")
+                                value=value['gvcp.bootstrap.custom.register.value']
+                            elif  'READREG' in j.split(':')[1]:
+                                value=i['_source']["layers"]["gvcp"][j].get("gvcp.bootstrap.custom.register.read_value")
+                            command_dict[result.get("Name")] = {x.tag.split('}')[1]: x.text for x in tree.find(text)}
+                            timestamp_dict[i['_source']["layers"]["frame"]['frame.time_relative']] = [j.split(':')[1],
+                                                                                                      {"value":int(value.split('x')[1].upper(),16) if value else None},
+                                                                                                      result.get("Name"),
+                                                                                                      command_dict[result.get("Name")].get('Description')]
     with open(args.dump[0:-5] + '_command.txt', 'w') as file:
         for k, v in command_dict.items():
             file.write(str(k) + ' ' + str(v) + '\n')
@@ -42,5 +55,6 @@ def parser(dumps, tree):
         for k, v in timestamp_dict.items():
             file.write(str(k) + ' ' + str(v) + '\n')
     print('finished')
-parser(dumps, tree)
 
+
+parser(dumps, tree)
