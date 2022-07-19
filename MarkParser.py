7ba1604793d4a19c6406485d5ccda68bebe3d7eb
root <Zao0oza>
2022-07-19 13:24:51 +0300

    some fixes

--- MarkParser.py
+++ MarkParser.py
@@ -23,6 +23,12 @@ with open(args.dump) as f:
 result = tree.find(
     './*[{http://www.genicam.org/GenApi/Version_1_1}StructReg]//*[@Comment="%s"]' % 'RemoveParameterLimitInqReg')
 
+features_dict={}
+with open('mer_features', 'r') as f:
+    for string in f:
+        s=string.split("#")
+        name=s[0].split('=')
+        features_dict[name[0]]=[name[1].replace(" ",''),s[1]]
 
 def parser(dumps, tree, manual):
     for x in tree.findall('.//*{http://www.genicam.org/GenApi/Version_1_1}Formula'):
@@ -64,7 +70,6 @@ def parser(dumps, tree, manual):
                             value = 'OpenAccess'
                         elif i['_source']["layers"][protocol][j].get("gvcp.bootstrap.control.controlaccess") == "1":
                             value = 'ControlAccess'
-                            print(45)
                         elif i['_source']["layers"][protocol][j].get("gvcp.bootstrap.control.exclusiveaccess") == "1":
                             value = 'ExclusiveAccess'
                         else:
@@ -91,10 +96,15 @@ def parser(dumps, tree, manual):
                                 value = i['_source']["layers"]["gvcp"][j].get(
                                     "gvcp.bootstrap.custom.register.read_value")
                             command_dict[result.get("Name")] = {x.tag.split('}')[1]: x.text for x in tree.find(text)}
-                            timestamp_items = [j.split(':')[1],
+                            timestamp_items = [j.split(':')[1],{"address":res},
                                                {"value": int(value.split('x')[1].upper(), 16) if value else None},
                                                {result.get("Name"): command_dict[result.get("Name")].get('Description')}
                                                ]
+                            simple_comands=[j.split(':')[1],result.get("Name"),{"address":res},
+                                               {"value": int(value.split('x')[1].upper(), 16) if value else None}]
+                            if simple_comands:
+                                simple_comand_dict[timestamp_time] = simple_comands
+                                simple_comands = None
 
                     else:
                         for key in i['_source']["layers"][protocol][j].keys():
@@ -138,33 +148,35 @@ def parser(dumps, tree, manual):
                                                    {"value": value},
                                                    {result.get("Name"): command_dict[result.get("Name")]}
                                                    ]
-                                simple_comands = ['read' if "read" in j else 'write', result.get("Name"), value,
+                                simple_comands = ['read' if "read" in j else 'write', result.get("Name"), {"value": value},
                                                   command_dict[result.get("Name")].get('Description') if command_dict[
-                                                      result.get("Name")].get('Description') else None]
+                                                      result.get("Name")].get('Description') else None, {"address":res}]
                             else:
                                 if formula_res:
                                     timestamp_items = [j,
-                                                       {result.get("Name"): command_dict[result.get("Name")]},
+                                                       result.get("Name"),
                                                        {'formula': formula_res}
                                                        ]
-                                    simple_comands = ['read' if "read" in j else 'write', result.get("Name"),
-                                                      formula_res]
+                                    simple_comands = ['read' if "read" in j else 'write', result.get("Name"),{"address":res},
+                                                      formula_res,]
                                 else:
                                     timestamp_items = [j,
-                                                       {result.get("Name"): command_dict[result.get("Name")]}
+                                                       result.get("Name")
                                                        ]
-                                    simple_comands = ['read' if "read" in j else 'write', result.get("Name")]
+                                    simple_comands = ['read' if "read" in j else 'write', result.get("Name"),{"address":res}]
+
+
                     else:
                         timestamp_items = [j, i['_source']["layers"][protocol][j]]
                         # timestamp_items = None
-                        simple_comands = None
+                        simple_comands = [j, i['_source']["layers"][protocol][j]]
                 elif 'stream_leader' in j:
                     timestamp_items = i['_source']["layers"][protocol][j]
-                    simple_comands = j
+                    simple_comands = i['_source']["layers"][protocol][j]
 
                 else:
                     timestamp_items = j
-                    simple_comands = None
+                    simple_comands = i['_source']["layers"][protocol][j]
                 if begin and finished:
                     if begin == finished:
                         timestamp_dict[finished] = 'UDP'
@@ -177,7 +189,7 @@ def parser(dumps, tree, manual):
                     else:
                         timestamp_dict[begin_gvsp] = 'GVSP_BEGIN'
                         timestamp_dict[finished_gvsp] = {"GVSP_FINISHED": counter_gvsp}
-                begin, finished, begin_gvsp, finished_gvsp, formula_res, result_reg = None, None, None, None, None, None
+                begin, finished, begin_gvsp, finished_gvsp, formula_res, result_reg,res = None, None, None, None, None, None, None
                 counter, counter_gvsp = 0, 0
 
                 timestamp_dict[timestamp_time] = timestamp_items
@@ -187,8 +199,12 @@ def parser(dumps, tree, manual):
             timestamp_dict[timestamp_time] = "USB"
 
     with open(args.dump[0:-5] + '_command.txt', 'w') as file:
-        for k in sorted(command_dict.keys()):
-            file.write(str(k) + ' ' + str(command_dict[k]) + '\n')
+        for k in sorted(timestamp_dict.keys()):
+            string = ""
+            for j in timestamp_dict[k]:
+                if j:
+                    string += str(j) + ' '
+            file.write(string + '\n')
     with open(args.dump[0:-5] + '_simple_command.txt', 'w') as file:
 
         for k in sorted(simple_comand_dict.keys()):
